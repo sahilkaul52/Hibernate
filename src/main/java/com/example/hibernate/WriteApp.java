b334c6b671eebea3e8e62c94256db46b2bc833a2
sahilkaul52 <sahilkaul52@gmail.com>
2024-03-03 16:46:42 +0530

    first hibernateApp commit

---
+++ src/main/java/com/example/hibernate/WriteApp.java
@@ -0,0 +1,62 @@
+package com.example.hibernate;
+
+import java.lang.module.Configuration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.bytecode.internal.bytebuddy.PrivateAccessorException;
+
+import com.sahil.hibernate.entities.Student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class WriteApp {
+
+	public static void main(String[] args) {
+
+		org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+
+		configuration.configure();
+
+		configuration.addAnnotatedClass(Student.class);
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+		Student student = new Student();
+		student.setName("Sum");
+		student.setOrigin("kashmir");
+		student.setEmail("ks@hotmail.com");
+		student.setUserId(1);
+
+		Student student2 = new Student();
+		student2.setName("Priti");
+		student2.setOrigin("jammu");
+		student2.setEmail("pb@gmail.com");
+
+		
+		 session.beginTransaction();
+		 
+		 session.persist(student);
+		  
+		 session.getTransaction().commit();
+		 
+		  session.close();
+		 
+
+		Session session2 = sessionFactory.openSession();
+
+		session2.beginTransaction();
+
+		session2.persist(student2);
+
+		session2.getTransaction().commit();
+
+		session2.close();
+
+	}
+
+}
---
+++ src/main/java/com/sahil/hibernate/entities/Student.java
@@ -0,0 +1,57 @@
+package com.sahil.hibernate.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student 
+{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	
+    @Column(name="NAME", length=50)
+	private String name;
+	
+	private String email;
+	
+	private String origin;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+}
